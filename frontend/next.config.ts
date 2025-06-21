db3435e0eee45b7f59337ad982be1b8079a27d7f
MoriguchiHinata <forest.tp03@gmail.com>
2025-06-21 15:05:46 +0900

    feat: GitHub Pages にデプロイ

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/chordlens',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
