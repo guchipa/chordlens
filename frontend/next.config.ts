c34a152afd22e9c5a3b88da74f025cb070201829
MoriguchiHinata <forest.tp03@gmail.com>
2025-06-25 17:32:59 +0900

    add: vercel デプロイ用のファイル追加

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,25 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+
+  compiler: {
+    styledComponents: false,
+    emotion: false,
+  },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader', 
+        ],
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
