be703c39654e482c140311151f0dd41478f8ed59
MoriguchiHinata <forest.tp03@gmail.com>
2025-06-25 17:08:49 +0900

    chore: 名称変更

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -197,7 +197,7 @@ export default function HomePage() {
       <main className="container mx-auto flex flex-grow flex-col items-center gap-8 p-4 sm:p-8 md:p-12">
         <div className="text-center">
           <h1 className="text-3xl font-extrabold text-gray-800 sm:text-4xl">
-            和音チューナー Web版
+            和音チューナー
           </h1>
           <h2 className="mt-2 max-w-2xl text-base text-gray-600 sm:text-lg">
             マイクから音声を入力し、設定した和音の純正律からの音程のズレをリアルタイムで解析します。
