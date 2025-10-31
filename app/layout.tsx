135144bc388a275eb5dfe86bb981934bfaa14d44
MoriguchiHinata <forest.tp03@gmail.com>
2025-11-01 00:27:51 +0900

    feat: add update notification component for service worker updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { UpdateNotification } from "@/components/feature/UpdateNotification";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -46,6 +47,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <UpdateNotification />
       </body>
     </html>
   );
